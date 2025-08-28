e0874ab216a0cc7ef7bba0fc906faa25900530d1
19950512 <mattmaydana@gmail.com>
2025-08-27 23:55:16 -0300

    f

--- packages/infrastructure/src/database/repositories/PrismaBusinessRepository.ts
+++ packages/infrastructure/src/database/repositories/PrismaBusinessRepository.ts
@@ -12,7 +12,7 @@ export class PrismaBusinessRepository implements BusinessRepository {
           document: business.document.toString(),
           phone: business.phone.toString(),
           whatsapp: business.whatsapp.toString(),
-          logoUrl: business.logoUrl?.toString(),
+          logoUrl: business.logoUrl.toString(),
           createdAt: new Date(),
           updatedAt: new Date(),
         },
@@ -48,7 +48,7 @@ export class PrismaBusinessRepository implements BusinessRepository {
         document: business.document.toString(),
         phone: business.phone.toString(),
         whatsapp: business.whatsapp.toString(),
-        logoUrl: business.logoUrl?.toString(),
+        logoUrl: business.logoUrl.toString(),
         // Deixar que o banco defina createdAt e updatedAt automaticamente
         // createdAt e updatedAt são definidos pelo schema do Prisma com @default(now()) e @updatedAt
       }
@@ -64,7 +64,7 @@ export class PrismaBusinessRepository implements BusinessRepository {
         document: business.document.toString(),
         phone: business.phone.toString(),
         whatsapp: business.whatsapp.toString(),
-        logoUrl: business.logoUrl?.toString(),
+        logoUrl: business.logoUrl.toString(),
         updatedAt: new Date(),
       }
     });
@@ -91,7 +91,7 @@ export class PrismaBusinessRepository implements BusinessRepository {
       document: data.document,
       phone: data.phone,
       whatsapp: data.whatsapp,
-      logoUrl: data.logoUrl?.toString(),
+      logoUrl: data.logoUrl.toString(),
       createdAt: data.createdAt?.toISOString(),
       updatedAt: data.updatedAt?.toISOString(),
     });
